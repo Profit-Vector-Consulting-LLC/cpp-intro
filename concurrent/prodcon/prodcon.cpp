3bd82948fc54bc74b25228486ad03ca7c2761ac2
EC2 Default User <ec2-user@ip-172-31-29-190.ec2.internal>
2025-11-10 10:25:18 +0000

    Reformatted code.  Added clean shutdown after 10 seconds.

--- concurrent/prodcon/prodcon.cpp
+++ concurrent/prodcon/prodcon.cpp
@@ -1,16 +1,14 @@
+#include <chrono>
 #include <condition_variable>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <thread>
 #include <mutex>
 #include <queue>
-#include <chrono>
+#include <string>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
-
-int queSize = 20;
 static volatile bool isDone = false;
 
 mutex queue_access;
@@ -19,66 +17,74 @@ mutex wake_up;
 condition_variable cv;
 queue<int> que;
 
+void producer(int id) {
+    cout << "Producer Starting...\n";
 
-void producer() {
-        cout << "Producer Starting...\n";
+    int i = 0;
+    int count = 0;
 
-        int i = 0;
-	int count = 0;
-        while(!isDone) {
-		this_thread::sleep_for(chrono::milliseconds(250));
-                lock_guard<mutex> lk(queue_access);
-		int n = (41*count)%13+1;
-		for(int j=0; j<n; j++){
-		  que.push(i++);
-		}
-		count = (count+1)%1000000;
-                cv.notify_one();
+    while (!isDone) {
+        this_thread::sleep_for(chrono::milliseconds(250));
+
+        {
+            lock_guard<mutex> lk(queue_access);
+            int n = (41 * count) % 13 + 1;
+            for (int j = 0; j < n; j++) {
+                que.push(i++);
+            }
+            count = (count + 1) % 1000000;
         }
 
+        cv.notify_one();
+    }
+    cout << "Producer #" << id << " shutting down." << endl;
 }
 
 void consumer(int id) {
-        cout << "Consumer #" << id << " Starting...\n";
-        int i = 0;
-        do  {
-                unique_lock<mutex> lk(wake_up);
-                cv.wait(
-                                lk,
-                                [] {
-                                        return !que.empty() || isDone;
-                                }
-                        );
+    cout << "Consumer #" << id << " Starting...\n";
 
-                cout << "polling..." << endl;
-                if(!que.empty()) {
-			lock_guard<mutex> lk2(queue_access);
-                        i = que.front();
-                        que.pop();
-                        cout << "Cosumer #" << id << ": " << i << ", " << "queue size: " << que.size() << "\n";
-		        this_thread::sleep_for(chrono::milliseconds(1));
-                }
-                lk.unlock();
-        } while(!isDone);
+    int i = 0;
+    do {
+        unique_lock<mutex> lk(wake_up);
+        cv.wait(lk, [] { return !que.empty() || isDone; });
 
+        if (!que.empty()) {
+            lock_guard<mutex> lk2(queue_access);
+            i = que.front();
+            que.pop();
+            cout << "Cosumer #" << id << ": " << i << ", "
+                 << "queue size: " << que.size() << "\n";
+            this_thread::sleep_for(chrono::milliseconds(1));
+        }
+
+        lk.unlock();
+    } while (!isDone);
+    
+    cout << "Consumer #" << id << " shutting down." << endl;
 }
 
 int main() {
+    thread p[3];
+    for(int i=0; i<3; i++){
+	p[i] = thread(&producer, i);
+    }
 
-        thread p(&producer);
+    thread c[10];
+    for (int i = 0; i < 10; i++) {
+        c[i] = thread(&consumer, i);
+    }
 
-	thread c[10];
-        for(int i = 0; i < 10; i++)
-                c[i] = thread(&consumer, i);
+    this_thread::sleep_for(chrono::milliseconds(10*1000));
+    cout << "Terminating..." << endl;
+    isDone = true;
 
-        p.join();
-        for(int i = 0; i < 3; i++)
-                c[i].join();
-
-
-        isDone = true;
-
-        return 0;
+    for(int i=0; i<3; i++){
+	p[i].join();
+    }
+    cv.notify_all();
+    for (int i = 0; i < 10; i++) {
+        c[i].join();
+    }
+    return 0;
 }
-
 
